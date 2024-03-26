1273b7da44cc06697d3e85599482114b3934eded
ganesh <desaiganeshkumar13@gmail.com>
2024-03-26 12:31:14 +0530

    added pipelines for frontend setup

--- ansible.sh
+++ ansible.sh
@@ -1,3 +1,2 @@
 component=$1
-ansible-playbook -i $component-dev.ganeshreddy12.online, -e ansible_user=ec2-user -e ansib
-le_password=DevOps321 expense.yml -e env=dev -e role_name=$component
+ansible-playbook -i $component-dev.ganeshreddy12.online, -e ansible_user=ec2-user -e ansible_password=DevOps321 -e env=dev -e role_name=$component expense.yml
